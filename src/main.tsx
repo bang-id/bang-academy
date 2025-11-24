36bace8993979580e15104ee050e3640ea0bfa53
Daybreak <bsrohith2002@gmail.com>
2025-11-24 12:29:06 +0530

    integrated vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import './styles/global.css'
 
@@ -33,5 +34,6 @@ window.scrollTo(0, 0)
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 )
